15926ba3e5eb7ebc0051a28c73adead36018f40d
jortegajr22 <jortega22@miners.utep.edu>
2025-02-19 12:47:37 -0700

    gitgud

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,23 +1,51 @@
 #include "tokenizer.h"
 #include "stdlib.h"
 
+
 int space_char(char c)
 {
-  return (c==' ') ?  1 : 0;
+  return (c == ' ' || c == '\t');
 }
 
 int non_space_char(char c)
 {
-  return  (c != ' ') ? 1 : 0;
+  return  !space_char(c) && c != '\0';
 }
 
-char *token_start(char *str);
+char *token_start(char *str)
+{
+  while (*str != '\0' && space_char(*str)){
+    str++;
+  }
+  return (*str == '\0') ? NULL : str;
+}
 
-char *token_terminator(char *token);
+char *token_terminator(char *token)
+{
+  while (non_space_char(*token)){
+    token++;
+  }
+  return token
+}
 
-int count_tokens(char *str);
+int count_tokens(char *str)
+{
+  int count = 0;
+    char *token = token_start(str);
+  while (non_space_char(*token)){
+    count++;
+    token = token_start(token_terminator(token));
+  }
+  return count;
+}
 
-char *copy_str(char *inStr, short len);
+char *copy_str(char *inStr, short len)
+{
+  char *copy_str = (char *)malloc((len +1) *sizeof(char));
+
+  for(short i = 0; i < len; i++){
+    copy_str[1]
+}
 
 char **tokenize(char *str);
 
---
+++ src/uimain.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+
+int main()
+{
+  char input[100];
+  puts("Hello,there!\n");
+
+  printf("Please enter a string, type 'history', 'history #', or exit: \n");
+  fgets(input, sizeof(input), stdin);
+
+  printf("You entered: %s \n", input);
+  return 0;
+}
